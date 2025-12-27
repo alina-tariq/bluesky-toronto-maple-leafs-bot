e6d6e33f9fc73a3abf769c61b3a3c36625ef6322
Alina <71248697+alina-tariq@users.noreply.github.com>
2025-12-26 20:03:37 -0500

    Log full error response in getPostText function

--- src/lib/getPostText.ts
+++ src/lib/getPostText.ts
@@ -135,9 +135,9 @@ export default async function getPostText()
 
 		return urlsStringsAltsCards; // Return this singular concatenated string. 
 	} catch (error: any) {
-		console.log(error.response.headers);
+		console.log(error.response);
 		// var resetTime = parseInt(error.response.headers['x-rate-limit-reset']);
 		// console.log(timeDifference(new Date(resetTime * 1000)));
-		throw error.response.headers;
+		throw error.response;
 	}
 }
